5127f7b630deb37a461bdfd3cf75c2d0aa97da18
Alicja Namieta <anamieta@3-C-5.42heilbronn.de>
2024-01-10 14:31:46 +0100

    strspn

--- Level 2/ft_strspn.c
+++ Level 2/ft_strspn.c
@@ -6,21 +6,43 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 14:41:23 by anamieta          #+#    #+#             */
-/*   Updated: 2024/01/09 14:47:30 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/01/10 14:31:07 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+char *ft_strchr(const char *s, int c)
+{
+    int i = 0;
+    while (s[i])
+    {
+        if (s[i] == c)
+            return ((char *)&s[i]);
+        i++;
+    }
+    return 0;
+}
 
 size_t	ft_strspn(const char *s, const char *accept)
 {
     int i = 0;
-    int j = 0;
     while (s[i])
     {
-        j = 0;
-        while (accept[j])
-        {
-            if (s[i] == accept[j])
-                
-        }
+        if (ft_strchr(accept, s[i]) == 0)
+            break;
+        i++;
     }
+    return i;
+}
+
+int main(void)
+{
+    char str[] = "Aho, let me be free";
+    char check[] = "";
+    size_t myfun = ft_strspn(str, check);
+    size_t deffun = strspn(str, check);
+    printf("My function: %zu\nDefault function: %zu\n", myfun, deffun);
+    return 0;
 }
