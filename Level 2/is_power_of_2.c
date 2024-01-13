b717d29ff2c8be533d71c1e69120e2c3046baa77
Alicja Namieta <anamieta@3-C-5.42heilbronn.de>
2024-01-13 19:47:33 +0100

    nothing

---
+++ Level 2/is_power_of_2.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_power_of_2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 16:29:20 by anamieta          #+#    #+#             */
+/*   Updated: 2024/01/13 16:53:18 by anamieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int is_power_of_2(unsigned int n)
+{
+    if (n == 0)
+        return 0;
+    while (n % 2 == 0)
+        n /= 2;
+    if (n == 1)
+        return 1;
+    else
+        return 0;
+}
+
+int main(void)
+{
+    int n = 34;
+    int check = is_power_of_2(n);
+    printf("%d", check);
+}
--- Level 2/last_word.c
+++ Level 2/last_word.c
@@ -1,43 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   inter.c                                            :+:      :+:    :+:   */
+/*   last_word.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/10 18:49:06 by anamieta          #+#    #+#             */
-/*   Updated: 2024/01/13 15:47:01 by anamieta         ###   ########.fr       */
+/*   Created: 2024/01/13 16:53:46 by anamieta          #+#    #+#             */
+/*   Updated: 2024/01/13 19:11:49 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int iter(char *str, char c, int len)
+void    last_word(char *str)
 {
+    int    j = 0;
     int i = 0;
-    while (str[i] && (i < len || len == -1))
+
+    while (str[i])
     {
-        if (str[i] == c)
-            return 1;
+        if (str[i] == ' ' && str[i + 1] >= 33 && str[i + 1] <= 126)
+            j = i + 1;
         i++;
     }
-    return 0;
+    while (str[j] >= 33 && str[j] <= 127)
+    {
+        write(1, &str[j], 1);
+        j++;
+    }
 }
 
-int main(int argc, char *argv[])
+int        main(void)
 {
-    int i;
-
-    if (argc == 3)
-    {
-        i = 0;
-        while (argv[1][i])
-        {
-            if (!iter(argv[1], argv[1][i], i) && iter(argv[2], argv[1][i], -1))
-                write(1, &argv[1][i], 1);
-            i++;
-        }
-    }
+    char str[] = "this        ...       is sparta, then again, maybe    not";
+    last_word(str);
     write(1, "\n", 1);
     return (0);
 }
