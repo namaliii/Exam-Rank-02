8bbf74615b2261d9ff5fc880ddc7bea19b9f12b9
Alicja Namieta <anamieta@3-C-4.42heilbronn.de>
2024-01-25 13:28:21 +0100

    im printing bits no shittt

---
+++ Level 2/print_bits.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_bits.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 13:22:47 by anamieta          #+#    #+#             */
+/*   Updated: 2024/01/25 13:28:01 by anamieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void print_bits(unsigned char octet)
+{
+    int i = 8;
+
+    while (i > 0)
+    {
+        i--;
+        char bit = (octet >> i & 1) + '0';
+        write(1, &bit, 1);
+    }
+}
+
+int main(void)
+{
+    unsigned char hophop = 'a';
+    print_bits(hophop);
+    return 0;
+}
