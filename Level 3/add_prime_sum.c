3006d17c0c75ba90f79b99dfa1135401a4621010
Alicja Namieta <anamieta@3-D-4.42heilbronn.de>
2024-01-27 18:26:16 +0100

    add prime sum

--- Level 3/add_prime_sum.c
+++ Level 3/add_prime_sum.c
@@ -6,45 +6,43 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 18:45:25 by anamieta          #+#    #+#             */
-/*   Updated: 2024/01/27 14:29:09 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/01/27 18:24:43 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-#include <stdio.h>
-
-int ft_atoi(char *str)
+int ft_atoi(char *s)
 {
     int i = 0;
     int result = 0;
-    while (str[i])
+    while (s[i])
     {
-        result = result * 10 + (str[i] - '0');
+        result = result * 10 + (s[i] - '0');
         i++;
     }
     return (result);
 }
 
-int is_prime(int nbr)
+int is_prime(int num)
 {
     int i = 2;
-    if (nbr <= 1)
+    if (num <= 1)
         return 0;
-    while (i * i <= nbr)
+    while (i * i <= num)
     {
-        if (nbr % i == 0)
+        if (num % i == 0)
             return 0;
         i++;
     }
     return 1;
 }
 
-void put_nbr(int num)
+void put_nbr(int nbr)
 {
-    if (num >= 10)
-        put_nbr(num / 10);
-    char digit = num % 10 + '0';
+    if (nbr >= 10)
+        put_nbr(nbr / 10);
+    char digit = nbr % 10 + '0';
     write(1, &digit, 1);
 }
 
@@ -53,15 +51,14 @@ int main(int argc, char **argv)
     int sum = 0;
     if (argc == 2)
     {
-        int nbr = ft_atoi(argv[1]);
-        while (nbr > 0)
+        int n = ft_atoi(argv[1]);
+        while (n > 0)
         {
-            if (is_prime(nbr))
-                sum += nbr;
-            nbr--;
+            if (is_prime(n))
+                sum += n;
+            n--;
         }
     }
     put_nbr(sum);
     write(1, "\n", 1);
-    return 0;
 }
