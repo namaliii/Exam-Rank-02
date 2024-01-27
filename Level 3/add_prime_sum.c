819c2ea297283310485f1ccdafc7ecd6955671fb
Alicja Namieta <anamieta@3-D-4.42heilbronn.de>
2024-01-27 14:32:06 +0100

    little things make difference

--- Level 3/add_prime_sum.c
+++ Level 3/add_prime_sum.c
@@ -6,57 +6,62 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 18:45:25 by anamieta          #+#    #+#             */
-/*   Updated: 2024/01/26 17:31:48 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/01/27 14:29:09 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_atoi(char *s)
+#include <stdio.h>
+
+int ft_atoi(char *str)
 {
+    int i = 0;
     int result = 0;
-    while (*s)
-    {    
-        result = result * 10 + (*s - '0');
-        s++;
+    while (str[i])
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
     }
-    return result;
+    return (result);
 }
-int is_prime(int num)
+
+int is_prime(int nbr)
 {
     int i = 2;
-    if (num <= 1)
+    if (nbr <= 1)
         return 0;
-    while (i * i <= num)
+    while (i * i <= nbr)
     {
-        if (num % i == 0)
+        if (nbr % i == 0)
             return 0;
-        i++
+        i++;
     }
     return 1;
 }
 
-void put_nbr(int n)
+void put_nbr(int num)
 {
-    if (n >= 10)
-        put_nbr(n / 10);
-    char digit = n % 10 + '0';
+    if (num >= 10)
+        put_nbr(num / 10);
+    char digit = num % 10 + '0';
     write(1, &digit, 1);
 }
+
 int main(int argc, char **argv)
 {
+    int sum = 0;
     if (argc == 2)
     {
-        int num = ft_atoi(argv[1]);
-        int sum = 0;
-        while (num > 0)
+        int nbr = ft_atoi(argv[1]);
+        while (nbr > 0)
         {
-            if (is_prime(num))
-                sum += num;
-            num--;
+            if (is_prime(nbr))
+                sum += nbr;
+            nbr--;
         }
-        put_nbr(sum);
     }
+    put_nbr(sum);
     write(1, "\n", 1);
     return 0;
 }
