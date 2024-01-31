bc79acb8793848580d5d7bf6a8c91e31826e1224
Alicja Namieta <anamieta@3-D-4.42heilbronn.de>
2024-01-31 20:24:12 +0100

    splittin

--- Level 4/ft_split.c
+++ Level 4/ft_split.c
@@ -6,23 +6,22 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 12:05:52 by anamieta          #+#    #+#             */
-/*   Updated: 2024/01/24 16:28:03 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/01/31 20:23:56 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
-char *ft_strncpy(char *s1, char *s2, int n)
+char *ft_strncpy(char *dst, char *src, int n)
 {
     int i = 0;
-    while (i < n && s2[i])
+    while (src[i] && i < n)
     {
-        s1[i] = s2[i];
+        dst[i] = src[i];
         i++;
     }
-    s1[i] = '\0';
-    return s1;
+    dst[i] = '\0';
+    return dst;
 }
 
 char **ft_split(char *str)
@@ -31,23 +30,26 @@ char **ft_split(char *str)
     int j = 0;
     int k = 0;
     int word_count = 0;
+    
     while (str[i])
     {
         while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
             i++;
         if (str[i])
             word_count++;
-        while (str[i] && (str[i] != ' ' && str[i] != '\t' || str[i] != '\n'))
+        while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
             i++;
     }
     char **array = (char **)malloc(sizeof(char *) * (word_count + 1));
+    if (array == NULL)
+        return NULL;
     i = 0;
     while (str[i])
     {
         while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
             i++;
         j = i;
-        while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+        while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
             i++;
         if (i > j)
         {
@@ -57,5 +59,32 @@ char **ft_split(char *str)
         }
     }
     array[k] = NULL;
-    return (array);
+    return array;
 }
+
+// #include <stdio.h>
+
+// // Function prototypes
+// char *ft_strncpy(char *s1, char *s2, int n);
+// char **ft_split(char *str);
+
+// int main() {
+//     // Example usage of ft_split
+//     char input_str[] = "  This is a test string   ";
+//     char **result = ft_split(input_str);
+
+//     // Print the result
+//     printf("Original String: \"%s\"\n", input_str);
+//     printf("Split String:\n");
+
+//     // Iterate through the array of strings
+//     for (int i = 0; result[i] != NULL; ++i) {
+//         printf("[%d]: \"%s\"\n", i, result[i]);
+//         free(result[i]); // Free each individual string
+//     }
+
+//     // Free the array itself
+//     free(result);
+
+//     return 0;
+// }
