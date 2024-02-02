5d3c0145582428487a7fda07923858bd023ad19f
Alicja Namieta <anamieta@3-D-4.42heilbronn.de>
2024-02-02 20:54:28 +0100

    Euklidesie dopomoz

---
+++ Level 3/paramsum/paramsum.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 20:23:27 by anamieta          #+#    #+#             */
+/*   Updated: 2024/02/02 20:23:31 by anamieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putnbr(int n)
+{
+    char digit;
+
+    if (n >= 10)    
+        ft_putnbr(n / 10);
+    digit = (n % 10) + '0';
+    write(1, &digit, 1);
+}
+
+int main(int argc, char **argv)
+{
+    (void)argv;
+    
+    ft_putnbr(argc - 1);
+    write(1, "\n", 1);
+    return 0;
+}
