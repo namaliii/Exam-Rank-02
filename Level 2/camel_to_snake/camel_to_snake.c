5b96411015bb1ad8d7743fdb2050eb0b0aeea5f2
Alicja Namieta <anamieta@3-D-4.42heilbronn.de>
2024-02-07 14:08:37 +0100

    changes

--- Level 2/camel_to_snake/camel_to_snake.c
+++ Level 2/camel_to_snake/camel_to_snake.c
@@ -6,7 +6,7 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 13:33:40 by anamieta          #+#    #+#             */
-/*   Updated: 2024/01/06 19:00:42 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/02/07 14:08:09 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,20 @@
 
 int main(int argc, char **argv)
 {
-   int i = 0;
-   int upCaseCounter = 0;
-   if (argc == 2)
-   {
-        char *str = argv[1];
+    int i = 0;
+    char *str;
+    if (argc == 2)
+    {
+        str = argv[1];
         while (str[i])
         {
-            char current = str[i];
-            if (current >= 'A' && current <= 'Z')
+            if (str[i] >= 'A' && str[i] <= 'Z')
             {
-                upCaseCounter++;
+                str[i] = str[i] + 32;
+                write(1, "_", 1);
             }
+            write(1, &str[i], 1);
             i++;
         }
-        int snakeCaseLength = i + upCaseCounter + 1;
-        char *snakeCaseStr = (char *)malloc(snakeCaseLength);
-        if (snakeCaseStr == NULL)
-        {
-            write(1, "Memory allocation failed\n", 25);
-            return 1;
-        }
-        int snakeCaseIndex = 0;
-        i = 0;
-        while (str[i])
-        {
-            char current = str[i];
-            if (current >= 'A' && current <= 'Z')
-            {
-                snakeCaseStr[snakeCaseIndex] = '_';
-                snakeCaseStr[snakeCaseIndex + 1] = current + 32;
-                snakeCaseIndex++;
-            }
-            else
-            {
-                snakeCaseStr[snakeCaseIndex] = current;
-            }
-            snakeCaseIndex++;
-            i++;
-        }
-        snakeCaseStr[snakeCaseIndex] = '\0';
-        write(1, snakeCaseStr, snakeCaseLength);
-        free(snakeCaseStr);
-   }
+    }
 }
